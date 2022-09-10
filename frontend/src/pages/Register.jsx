e5b88f0b352dbda13821660bcfc3efb9864290fd
bishalr0y <bishalroy895@gmail.com>
2022-09-10 13:39:33 +0530

    add frontend ui

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,62 @@
+import {useState, useEffect} from 'react'
+import {FaUser} from 'react-icons/fa'
+
+const Register = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email:'',
+        password:'',
+        password2:''
+    })
+
+    const {name, email, password, password2} = formData
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        })
+        )
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    } 
+
+    return (
+    <>
+        <section className='heading'>
+            <h1>
+                <FaUser /> Register
+            </h1>
+            <p>Please create an account</p>
+        </section>
+
+        <section className='form'>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input type='text' className='formControl' id='name' name='name' value={name} placeholder='Enter your name' onChange={onChange}/>
+                </div>
+                <div className="form-group">
+                    <input type='email' className='formControl' id='email' name='email' value={email} placeholder='Enter your email' onChange={onChange}/>
+                </div>
+                <div className="form-group">
+                    <input type='password' className='formControl' id='password' name='password' value={password} placeholder='Enter your password' onChange={onChange}/>
+                </div>
+                <div className="form-group">
+                    <input type='password' className='formControl' id='password2' name='password2' value={password2} placeholder='Confirm password' onChange={onChange}/>
+                </div>
+
+                <div className="form-group">
+                    <button type='submit' className='btn btn-block'>
+                        Submit
+                    </button>
+                </div>
+               
+            </form>
+        </section>
+    </>
+  )
+}
+
+export default Register
