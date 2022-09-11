249537beee278b97fae7d9cea43e1d885076272a
bishalr0y <bishalroy895@gmail.com>
2022-09-11 14:00:31 +0530

    bug fix: deleting recipe doesn't reflect in the dashboard

--- frontend/src/features/recipes/recipeSlice.js
+++ frontend/src/features/recipes/recipeSlice.js
@@ -111,7 +111,7 @@ export const recipeSlice = createSlice({
                 state.isLoading = false
                 state.isSuccess = true
                 state.recipes = state.recipes.filter(
-                  (recipe) => recipe._id !== action.payload.id
+                  (recipe) => recipe._id !== action.payload._id
                 )
               })
               .addCase(deleteRecipe.rejected, (state, action) => {
